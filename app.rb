246cf6ec18e43a4f0b6adb21acbe3e5fe08cadf5
srappaport <sarah.rappaport@greendot.org>
2018-06-26 20:26:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ class App < Sinatra::Base
   end
 
   all_the_medicines = [
-    <Medicine:0x007fb739b1af88 @id=1, @name="penicillin" @group="antibiotic">,
-    <Medicine:0x007fb739b1af88 @id=2, @name="advil" @group="anti-inflammatory">,
-    <Medicine:0x007fb739b1af88 @id=3, @name="benadryl" @group="anti-histamine">
+    #<Medicine:0x007fb739b1af88 @id=1, @name="penicillin" @group="antibiotic">,
+    #<Medicine:0x007fb739b1af88 @id=2, @name="advil" @group="anti-inflammatory">,
+    #<Medicine:0x007fb739b1af88 @id=3, @name="benadryl" @group="anti-histamine">
   ]
 
   # Code your final two routes here:
