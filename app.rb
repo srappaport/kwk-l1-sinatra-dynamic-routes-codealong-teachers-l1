fa4e41a581b6e94d49750bfaf9191b9f35641346
srappaport <sarah.rappaport@greendot.org>
2018-06-26 20:28:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   ]
 
   # Code your final two routes here:
-  get '/medicines/:id' do
+  get "/medicines/:id" do
     @medicine = all_the_medicines.select do |medicine|
       medicine.id == params[:id]
     end.first
